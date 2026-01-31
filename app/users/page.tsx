fd482c14e478c2a4bfbb55dbe9008b461789c1ba
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2026-01-31 13:35:34 +0500

    Add Prisma integration and fetch users in UserPage
    Introduces a Prisma client setup in lib/prisma.ts and updates the users page to fetch and display user data from the database using Prisma.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,21 @@
 
-const UserPage = () => {
+import React from 'react'
+import prisma from '@/lib/prisma'
+
+const UserPage = async () => {
+  const users = await prisma.user.findMany()
+  
+
+    
   return (
-    <div>page</div>
+    <div>
+{users.map((user) => (
+  <div key={user.id}>
+    <h2>{user.name}</h2>
+    <p>{user.email}</p>
+  </div>  
+))}
+    </div>
   )
 }
 
