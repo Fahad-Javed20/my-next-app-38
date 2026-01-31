d28f59b98bb22f730204e343bacbd1d9a5a6b8e0
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2026-01-31 11:54:47 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'My Next App',
-  description: 'This is a sample Next.js application with custom metadata.',
+  title: "My Next App",
+  description: "This is a sample Next.js application with custom metadata.",
 };
+
+const cards = ["Card 1", "Card 2", "Card 3", "Card 4", "Card 5"];
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Welcome to My Next App</h1>
+      <div className="slider">
+        <div className="slide-track">
+          {[...cards, ...cards].map((card, index) => (
+            <div className="card" key={index}>
+              {card}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
